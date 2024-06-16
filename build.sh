8fb06389b18dbf13ddfce9571156ad65a20d63d1
Rodzpm <allan.leherpeuxprv@gmail.com>
2024-06-16 19:19:56 +0200

    feat: add chorus effect and fix adsr

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cp -r dev P7UREM
 sed -i.bak -e 's/inlet/adc/g' -e 's/outlet/dac/g' P7UREM/P7UREM.pd
-echo "#X connect 16 0 20 1;" >> P7UREM/P7UREM.pd
+echo "#X connect 15 0 19 1;" >> P7UREM/P7UREM.pd
 ./lib/Camomile/camomile -f P7UREM
 rm -rf P7UREM
